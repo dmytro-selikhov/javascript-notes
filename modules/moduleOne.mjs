8a52d2a3d333a28d096af82e90d50be95fdd640e
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-11-14 20:40:32 +0200

    several exports

---
+++ modules/moduleOne.mjs
@@ -0,0 +1,10 @@
+//moduleOne.js 	
+//const myName = ()=>{ 	
+//	console.log('Dima');	
+//}
+
+//export default myName;
+
+const sum = (a,b) => a + b;
+
+export default sum;
---
+++ modules/moduleTwo.mjs
@@ -0,0 +1,13 @@
+//moduleTwo.js
+// import printMyName from './moduleOne.js';
+// printMyName(); // Dima
+
+
+import printSum from './moduleOne.mjs';
+
+const res1 = printSum(10, 2);
+console.log(res1);
+
+
+const res2 = printSum(-2, 17);
+console.log(res2);
