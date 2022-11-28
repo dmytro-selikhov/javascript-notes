e19bc4b5c2668263dcb2f4d433785efa97556eb6
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-11-28 23:56:26 +0200

    task 42

--- practice-tasks.js
+++ practice-tasks.js
@@ -1136,18 +1136,98 @@ const posts = [
   { postId: 6134, commentsQuantity: 2 },
 ]
 
+
+function findPostById(id,i, posts){
+	let result;
+
+	posts.forEach((el) => {
+		if (el[i].postId === id){
+			result = el[i]
+			console.log(result);
+			return result;
+		} else {
+			result = undefined
+			console.log(result)
+			return result
+		}
+	})
+}
+
+
+
+
+console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
+
+console.log(findPostById(4511, posts)) // undefined
+
+
+// Mentors Solution
+
+function findPostById(postId, posts) {
+	return posts.find((post) => post.postId === postId)
+}
+
+
+
+const posts = [
+  { postId: 1355, commentsQuantity: 5 },
+  { postId: 5131, commentsQuantity: 13 },
+  { postId: 6134, commentsQuantity: 2 },
+]
+
+
+
+
 console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
 
 console.log(findPostById(4511, posts)) // undefined
 
 
 
+// task 42
+/*
+ *
+ * 1. Создайте функцию "arraySortInfo" с одним параметром - "inputArray"
+ *
+ * 2. Если хотя бы один элемент в массиве не является числом - вернуть "Некоторые элементы не являются числами"
+ *
+ * 3. Если числа в массиве отсортированы по возрастанию - вернуть "Массив отсортирован по возрастанию"
+ *
+ * 4. Если числа в массиве отсортированы по убыванию - вернуть "Массив отсортирован по убыванию".
+ *
+ * 5. Если массив не отсортирован - вернуть "Массив не отсортирован"
+ */
 
+const a = [5, 'abc', 10, 1]
+const b = [4, 10, 14, 25, 25, 50]
+const c = [150, 132, 80, 40]
+const d = [15, 26, 10, 23, 85]
 
+console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
+console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
+console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
+console.log(arraySortInfo(d)) // Массив не отсортирован
 
-
-
-
+/**
+ * ПОДСКАЗКИ
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Подсказка 1: Используйте метод массивов "every"
+ * Подсказка 2: Вы должны использовать два параметра в колбэк функции "element", "index"
+ * Подсказка 3: Каждый элемент массива, кроме первого, следует сравнивать с предыдущим
+ */
 
 
 
