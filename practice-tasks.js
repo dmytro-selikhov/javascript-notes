6b485b46e1a4afc629dad0a531a9e0632a503b6e
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-11-28 23:31:08 +0200

    task 41

--- practice-tasks.js
+++ practice-tasks.js
@@ -1055,7 +1055,7 @@ myCities.forEach((city, index) => console.log(cityInfo(city, index)))
 
 
 
-// task 39
+// task 40
 /* 
  * 1. Конвертируйте массив JSON объектов в массив объектов JavaScript
  *
@@ -1115,7 +1115,31 @@ console.log(postsJS[1].postId);
 console.log(postsJS[postsJS.length - 1].commentsQuantity);
 
 
-// task 40
+// task 41
+/*
+ *
+ * 1. Создайте функцию "findPostById" с двумя параметрами:
+ *  - ID поста
+ *  - массив постов
+ *
+ * 2. Функция должна вернуть пост с определенным ID
+ *
+ * 3. Если поста с определенным ID в массиве постов нет,
+ * функция должна вернуть "undefined"
+ *
+ * 4. Также внутри функции выведите в консоль ID поста
+ */
+
+const posts = [
+  { postId: 1355, commentsQuantity: 5 },
+  { postId: 5131, commentsQuantity: 13 },
+  { postId: 6134, commentsQuantity: 2 },
+]
+
+console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
+
+console.log(findPostById(4511, posts)) // undefined
+
 
 
 
