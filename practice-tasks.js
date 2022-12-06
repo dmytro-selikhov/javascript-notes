ccd37b8811adce928d07d472f0db9a9d89e7fa4e
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-06 23:28:19 +0200

    task 49

--- practice-tasks.js
+++ practice-tasks.js
@@ -1825,11 +1825,136 @@ const inputProducts = [
 ]
 
 console.log(quantitiesByCategories(inputProducts))
+
 /* {
   Accessories: 3,
   Phones: 1,
   Watches: 2
 } */
+
+
+
+// Solution
+
+function quantitiesByCategories(products){
+
+	products.reduce((finishObj, elements) => {
+		if (products.category === !finishObj.category){
+			finishObj.category == products.category
+			finishObj.category 
+		} else {
+
+		}
+	}, {})
+
+
+	return {}
+}
+
+	
+
+
+// Mentors Solution
+
+
+const inputProducts = [
+  {
+    title: 'Phone case',
+    price: 23,
+    quantity: 2,
+    category: 'Accessories',
+  },
+  {
+    title: 'Android phone',
+    price: 150,
+    quantity: 1,
+    category: 'Phones',
+  },
+  {
+    title: 'Headphones',
+    price: 78,
+    quantity: 1,
+    category: 'Accessories',
+  },
+  {
+    title: 'Sport Watch',
+    price: 55,
+    quantity: 2,
+    category: 'Watches',
+  },
+]
+
+
+function quantitiesByCategories(products) {
+	return products.reduce((qtysByCategories, product) =>{
+		const { category, quantity} = product
+		qtysByCategories[category] = (qtysByCategories[category] || 0) + quantity
+		
+		return qtysByCategories
+
+	}, {})
+}
+
+console.log(quantitiesByCategories(inputProducts))
+
+
+// task 49
+/*- Сортировка объектов в массиве
+ *
+ * 1. Создайте функцию "sortProductsByPrice" с одним параметром "products"
+ *
+ * 2. Эта функция должна отсортировать входной массив товаров
+ * по цене каждого товара в порядке возрастания
+ * и вернуть отсортированный массив
+ *
+ * 3. Оригинальный массив должен остаться без изменений
+ */
+
+const inputProducts = [
+  {
+    title: 'Phone case',
+    price: 23,
+    quantity: 2,
+    category: 'Accessories',
+  },
+  {
+    title: 'Android phone',
+    price: 150,
+    quantity: 1,
+    category: 'Phones',
+  },
+  {
+    title: 'Headphones',
+    price: 78,
+    quantity: 1,
+    category: 'Accessories',
+  },
+  {
+    title: 'Sport Watch',
+    price: 55,
+    quantity: 2,
+    category: 'Watches',
+  },
+]
+
+// const sortedProducts = sortProductsByPrice(inputProducts)
+
+// console.log(sortedProducts) // Массив отсортированных товаров
+
+// console.log(inputProducts) // Оригинальный массив не должен измениться
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
