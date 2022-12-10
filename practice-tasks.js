b07c4eae3157a824fc720bf459fc10125395097f
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-10 22:38:56 +0200

    task 51

--- practice-tasks.js
+++ practice-tasks.js
@@ -2017,6 +2017,114 @@ console.log(templateLiteral(myAnotherNumber))
 Это число больше или равно 10.
 Квадратный корень этого числа - 5.
 */
+ 
+
+
+// Solution
+
+const templateLiteral = (num) => {
+	if (num >= 10){
+		squarNum = Math.sqrt(num)
+		
+		return str = ` Число ${num}.
+		Это число больше или равно 10.
+		Квадратный корень этого числа - ${squarNum}
+		`
+	} else {
+		squarNum = Math.sqrt(num)
+		
+		return str = ` Число ${num}.
+		Это число меньше 10.
+		Квадратный корень этого числа - ${squarNum}
+		`
+	}
+} 
+
+
+const myNumber = 9
+console.log(templateLiteral(myNumber))
+/* 
+Число 9.
+Это число меньше 10.
+Квадратный корень этого числа - 3.
+*/
+
+// ТЕСТ 2
+const myAnotherNumber = 25
+console.log(templateLiteral(myAnotherNumber))
+
+
+
+
+
+//Mentors Solution
+
+
+const templateLiteral = (num) => {
+	return `Число ${num}.
+Это число ${num < 10 ? 'меньше' : 'больше или равно'} 10.
+Квадратный корень этого числа - ${Math.sqrt(num)}`
+}
+
+const myNumber = 9
+console.log(templateLiteral(myNumber))
+/* 
+Число 9.
+Это число меньше 10.
+Квадратный корень этого числа - 3.
+*/
+
+// ТЕСТ 2
+const myAnotherNumber = 25
+console.log(templateLiteral(myAnotherNumber))
+
+
+
+// task 51
+/*  Операторы "rest" (остаток) и "spread" (распространение)
+ *
+ * 1. Создайте функцию "meanScore",
+ * которая будет принимать любое количество аргументов,
+ * объединять их в один массив и возвращать среднее значение всех аргументов,
+ * округленное до 2 знаков после запятой.
+ *
+ * 2. Если хотя бы один элемент в этом массиве не является числом -
+ * выводим в консоль следующую ошибку:
+ * "Все аргументы в вызове функции должны быть числами!"
+ *
+ * ПОДСКАЗКА: В этом задании вы должны использовать как оператор "rest",
+ * так и оператор "spread".
+ */
+
+const scores1 = [0, 1.5, 2.5, 3.7]
+const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
+const scores3 = [1.3, 2.5, 1.9]
+const scores4 = ['abc', 1.3, true, 2.5, 1.9]
+
+console.log(meanScore(/* все элементы из массива "scores1" */))
+// 1.93
+
+console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+// 2.8
+
+console.log(
+  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
+)
+// 2.59
+
+console.log(meanScore(/* все элементы из массива "scores4" */))
+// Все аргументы в вызове функции должны быть числами!
+
+
+
+
+
+
+
+
+
+
+
 
 
 
