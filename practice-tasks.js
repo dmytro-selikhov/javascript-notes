c5dc3897ca84391060a2b3361a2f9e4b485efe23
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-13 21:48:11 +0200

    task 57

--- practice-tasks.js
+++ practice-tasks.js
@@ -2591,6 +2591,7 @@ console.log(min, max) // 18, 103
  * 2. Объект, возвращаемый функцией "personInfo", должен содержать только сокращенные имена свойств
  */
 
+
 const personInfo = (/* parameters */) => {
   /* return ... */
 }
@@ -2619,9 +2620,139 @@ console.log(result)
 */
 
 
+// My Solution
+const personInfo = (obj) => {
+	const {name, age: personAge, location: {country: origin, city: homeCity}, friendsQty = 0, createdAtYear = new Date().getFullYear()} = obj
+	
+   return {
+  	name,
+  	personAge,
+  	origin,
+  	homeCity,
+  	friendsQty,
+  	createdAtYear
+  }
+}
+
+const person = {
+  name: 'Alice',
+  age: 19,
+  location: {
+    country: 'England',
+    city: 'London',
+  },
+}
+
+const result = personInfo(person)
+
+console.log(result)
+/*
+{
+  name: "Alice",
+  personAge: 19,
+  origin: "England",
+  homeCity: "London",
+  friendsQty: 0,
+  createdAtYear: *current year*
+}
+*/
 
 
 
+
+// Mentors Solution
+
+
+const personInfo = ({name, age: personAge, location: {country: origin, city: homeCity}, friendsQty = 0, createdAtYear = new Date().getFullYear()}) => {
+	
+  return {
+  	name,
+  	personAge,
+  	origin,
+  	homeCity,
+  	friendsQty,
+  	createdAtYear
+  }
+}
+
+const person = {
+  name: 'Alice',
+  age: 19,
+  location: {
+    country: 'England',
+    city: 'London',
+  },
+}
+
+const result = personInfo(person)
+
+console.log(result)
+/*
+{
+  name: "Alice",
+  personAge: 19,
+  origin: "England",
+  homeCity: "London",
+  friendsQty: 0,
+  createdAtYear: *current year*
+}
+*/
+
+
+// task 57
+/*
+- Трансформация массива объектов
+ *
+ * 1. Создайте функцию "processPosts", которая будет возвращать новый массив сообщений
+ *
+ * 2. Обратите внимание, что
+ *  - некоторые имена свойств в каждом сообщении изменены
+ *  - ID каждого сообщения увеличен на 1000
+ *
+ * 3. Исходный массив постов должен остаться без изменений
+ */
+
+const testPosts = [
+  {
+    postId: 234,
+    author: 'robd',
+    commentsQty: 5,
+  },
+  {
+    postId: 823,
+    author: 'sady',
+  },
+  {
+    postId: 161,
+    author: 'merryl',
+    commentsQty: 8,
+  },
+]
+
+const processedPosts = processPosts(testPosts)
+console.log(processedPosts)
+/*
+[
+  {
+    postId: 1234,
+    postAuthor: 'robd',
+    postCommentsQty: 5
+  },
+  {
+    postId: 1823,
+    postAuthor: 'sady',
+    postCommentsQty: 0
+  },
+  {
+    postId: 1161,
+    postAuthor: 'merryl',
+    postCommentsQty: 8
+  }
+]
+*/
+
+console.log(testPosts)
+// оригинальный массив должен остаться без изменений
 
 
 
