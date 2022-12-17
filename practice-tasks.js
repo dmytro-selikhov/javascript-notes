5ebef2fa259ce9a2f8b0fd4bfb1c62cd86f09a3f
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-17 21:42:19 +0200

    task 71

--- practice-tasks.js
+++ practice-tasks.js
@@ -3641,8 +3641,49 @@ console.log(c)
 // [3, 4, 5, 6, 7]
 
 
+// task 71
+
+/* Spread Operator
+ *
+ * Используя оператор "spread" создайте новый массив
+ * Вывод в консоли должен быть таким же как в конце задания
+ */
+
+const a = [1, 2]
+const b = [4, 5]
+const c = [8, 9, 10]
+const d = 11
+
+// Напишите код здесь
+
+console.log(combinedArray)
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+// Solution 
+const a = [1, 2]
+const b = [4, 5]
+const c = [8, 9, 10]
+const d = 11
+
+const combinedArray = [0, ...a, 3, ...b, 6, 7, ...c, 11]
+// Напишите код здесь
+
+console.log(combinedArray)
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 
+
+// Mentors Solution
+const a = [1, 2]
+const b = [4, 5]
+const c = [8, 9, 10]
+const d = 11
+
+const combinedArray = [0, ...a, 3, ...b, 6, 7, ...c, 11]
+// Напишите код здесь
+
+console.log(combinedArray)
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 
 
