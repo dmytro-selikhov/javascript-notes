7dd1816ef6f09ec39b192d533b708f3f7463285c
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-18 22:30:15 +0200

    task 79

--- practice-tasks.js
+++ practice-tasks.js
@@ -4182,13 +4182,74 @@ console.log(orange.priceInfo())
 
 // Solution
 
+class Fruit {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  priceInfo() {
+    return `Price of the ${this.name} is ${this.price}$`
+  }
+}
+
+
+const apple = new Fruit('Apple', 2)
+console.log(apple.priceInfo())
+// Price of the Apple is 2$
+
+const orange = new Fruit('Orange', 3)
+console.log(orange.priceInfo())
+// Price of the Orange is 3$
 
 
 
+// Mentors Solution
+
+ 
+class Fruit {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  priceInfo() { 
+    return `Price of the ${this.title} is ${this.price}$`
+  }
+}
 
 
+const apple = new Fruit('Apple', 2)
+console.log(apple.priceInfo())
+// Price of the Apple is 2$
+
+const orange = new Fruit('Orange', 3)
+console.log(orange.priceInfo())
+// Price of the Orange is 3$
 
 
+// task 79
+/* Перебор свойств объекта
+ *
+ * 1. Создайте функцию "sumObjectValues", которая будет суммировть
+ * все значения свойств, которые являются числами.
+ *
+ * 2. Сумму чисел необходимо вернуть из функции
+ *
+ * 3. Убедитесь, что итерация выполняется только
+ * по собственным свойствам объекта
+ */
+
+const objectWithNumbers = {
+  a: 10,
+  b: 20,
+  c: 'string',
+  d: 12,
+}
+
+// Создайте функцию здесь
+
+const result = sumObjectValues(objectWithNumbers)
+console.log(result)
+//42
 
 
 
